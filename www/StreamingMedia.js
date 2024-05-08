1e96719dd2a5b78a69a2b9b2ed08f2f8a4a7bbb2
Raffaele Salutari <raffaele.salutari@gmail.com>
2024-05-08 11:20:43 +0200

    Update StreamingMedia.js

--- www/StreamingMedia.js
+++ www/StreamingMedia.js
@@ -27,6 +27,10 @@ StreamingMedia.prototype.playVideo = function (url, options) {
 	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playVideo", [url, options]);
 };
 
+StreamingMedia.prototype.stopVideo = function (url, options) {
+	options = options || {};
+	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "stopVideo", [url, options]);
+};
 
 StreamingMedia.install = function () {
 	if (!window.plugins) {
