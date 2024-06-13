10456741e176794ea2868a541faacdaf2f86e9f1
Raffaele Salutari <raffaele.salutari@gmail.com>
2024-06-13 15:28:54 +0200

    Update StreamingMedia.js

--- www/StreamingMedia.js
+++ www/StreamingMedia.js
@@ -2,26 +2,6 @@
 function StreamingMedia() {
 }
 
-StreamingMedia.prototype.playAudio = function (url, options) {
-	options = options || {};
-	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playAudio", [url, options]);
-};
-
-StreamingMedia.prototype.pauseAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "pauseAudio", [options]);
-};
-
-StreamingMedia.prototype.resumeAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "resumeAudio", [options]);
-};
-
-StreamingMedia.prototype.stopAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "stopAudio", [options]);
-};
-
 StreamingMedia.prototype.playVideo = function (url, options) {
 	options = options || {};
 	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playVideo", [url, options]);
